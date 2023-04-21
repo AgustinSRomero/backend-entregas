ee79e5ab7192d2c0afcee4f0bba1f9db500d22cd
Agustin <romero.s.agustin@outlook.com>
2023-04-21 13:35:29 -0300

    RealtimeProd

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const productRouter = require('./routes/products.router')
 const cartRouter = require('./routes/carts.router')
 const viewsRouter = require('./routes/views.router')
+const { Server } = require('socket.io')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
@@ -15,11 +17,23 @@ app.set('view engine', 'handlebars')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/static', express.static(__dirname+'/public'))
+app.use(cookieParser())
 
 app.use('/', viewsRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).send('We are having trouble with the server')
+})
+
+app.get('/realTimeProducts', (req, res) => {
+    res.render('realTimeProducts', {})
+})
+
 const PORT = 8080
 
-app.listen(PORT, () => {console.log("Listening in port 8080")})
+const httpServer = app.listen(PORT, () => {console.log("Listening in port 8080")})
+
+const socketServer = new Server(httpServer)
---
+++ src/public/js/realTimeProd.js
@@ -0,0 +1,1 @@
+console.log("Realtime Products JS cargó")
